00e6afa01dd210c0f5e767b021b6681144a2f971
Leonardo Henrique Lopes <leonardoo.lopes126@gmail.com>
2019-11-05 22:34:05 -0300

    Finish the project

---
+++ readme.md
@@ -0,0 +1,5 @@
+<h1>ReactJS Project</h1>
+<h2>ReactJS with GitHub API Connection.</h2>
+<p>Search and list GitHub Repositories.</p>
+<p>For clone: git clone https://github.com/lhlleonardo/lhlleonardo-GoStack-ReactApp.git</p>
+<p>Course by: RocketSeat</p>
---
+++ src/pages/Main/index.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import Container from '../../components/container';
+import { Form, SubmitButton, List } from './styles';
+
+export default class Main extends Component {
+    state = {
+      newRepo: '',
+      repositories: [],
+      loading: false,
+      error: null,
+    };
+
+    componentDidMount() {
+        const repositories = localStorage.getItem('repositories');
+
+        if (repositories) {
+          this.setState({ repositories: JSON.parse(repositories) });
+        }
+    }
+
+    componentDidUpdate(_, prevState) {
+        const { repositories } = this.state;
+
+        if (prevState.repositories !== repositories) {
+          localStorage.setItem('repositories', JSON.stringify(repositories));
+        }
+    }
+
+    handleInputChange = e => {
+        this.setState({ newRepo: e.target.value, error: null });
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        this.setState({ loading: true, error: false });
+
+        try {
+          const { newRepo, repositories } = this.state;
+
+          if (newRepo === '') throw 'Need a Repository!';
+
+          const hasRepo = repositories.find(r => r.name === newRepo);
+
+          if (hasRepo) throw 'Repositoty already exists!';
+
+          const response = await api.get(`/repos/${newRepo}`);
+
+          const data = {
+            name: response.data.full_name,
+          };
+
+          this.setState({
+            repositories: [...repositories, data],
+            newRepo: '',
+          });
+        } catch (error) {
+          this.setState({ error: true });
+        } finally {
+          this.setState({ loading: false });
+        }
+    };
+
+    render() {
+        const { newRepo, repositories, loading, error } = this.state;
+
+        return (
+          <Container>
+            <h1>
+              <FaGithubAlt />
+              Repositories
+            </h1>
+
+            <Form onSubmit={this.handleSubmit} error={error}>
+              <input
+                type="text"
+                placeholder="Add repository..."
+                value={newRepo}
+                onChange={this.handleInputChange}
+              />
+
+              <SubmitButton loading={loading}>
+                {loading ? (
+                  <FaSpinner color="#FFF" size={14} />
+                ) : (
+                  <FaPlus color="#FFF" size={14} />
+                )}
+              </SubmitButton>
+            </Form>
+
+            <List>
+              {repositories.map(repository => (
+                <li key={repository.name}>
+                  <span>{repository.name}</span>
+                  <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                    More Details
+                  </Link>
+                </li>
+              ))}
+            </List>
+          </Container>
+        );
+      }
+}
